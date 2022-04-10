cceeb303733b0c552d5f9f53bf2aeb22183e0b4a
Nwafor <nwaforemmanuel42@gmail.com>
2022-04-10 21:52:37 +0000

    Hello world 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  * main - Prints a random number and states whether
- *	   it is positive, negetive, or zero.
+ * it is positive, negetive, or zero.
  *
  * Return: Always 0.
  */
