e5a06105e2f7db5b2b3c7e97c5a3b6acb18e1001
Nwafor <nwaforemmanuel42@gmail.com>
2022-04-12 22:34:59 +0000

    project 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,6 +4,8 @@
  * Print_times_table - prints the times table of the imput,
  * starting with 0.
  *
+ * Function: Prints the times table
+ *
  * @n: The value of the times table to be printed.
  */
 void print_times_table(int n)
