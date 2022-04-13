374acaa2f11c6b97f85f77d56c08d5677d7243da
Nwafor <nwaforemmanuel42@gmail.com>
2022-04-13 23:49:00 +0000

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests the function the prints if integer is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negetive(i);
+
+	return (0);
+}
