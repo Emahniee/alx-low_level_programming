9b5bf9f88b403a9b790ce0d02ca0df61402f7b19
Nwafor <nwaforemmanuel42@gmail.com>
2022-04-20 21:21:29 +0000

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,20 +1,17 @@
 #include "main.h"
-#include <stdio.h>
-
-void reset_to_98(int *);
 
 /**
  * main - check the code
  * 
  * Return: Always 0.
  */
-int main(void)
+void reset_to_98 (int *n)
 {
 int n;
 
-	n = 402;
-		printf("%d\n", n);
-			reset_to_98(&n);
-				printf("%d\n", n);
-					return (0);
+n = 402;
+printf("n = %d\n", n);
+reset_to_98(&n);
+printf("n = %d\n", n);
+return (0);
 }
