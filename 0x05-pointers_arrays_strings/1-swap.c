fc643cbd7673989b1ae2db911665f53278defa1f
Nwafor <nwaforemmanuel42@gmail.com>
2022-04-20 22:05:11 +0000

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,10 +6,10 @@
  *
  * Return: Always 0.
  */
-void swap_int(int a, int b)
+int main(void)
 {
-	int a = 48;
-	int b = 96;
+	int a = 98;
+	int b = 42;
 	printf("a = %d, b = %d\n", a, b);
 	 swap_int(&a, &b);
 	printf("a = %d, b = %d\n", a, b);
