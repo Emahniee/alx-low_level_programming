07c26f89dbe7851e011ce8ad6e30b3f775a862ed
Nwafor <nwaforemmanuel42@gmail.com>
2022-04-24 21:58:25 +0000

    Advanced task 5

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -25,25 +25,25 @@ j = size - o < 10 ? size - o : 10;
 printf("%08x: ", o);
 for (i = 0; i < 10; i++)
 {
-																					if (i < j)
-																					printf("%02x", *(b + o + i));
-																					else
-																					printf("  ");
-																					if (i % 2)
-																					{
-																					printf(" ");
-																					}
-																					}
+if (i < j)
+printf("%02x", *(b + o + i));
+else
+printf("  ");
+if (i % 2)
+{
+printf(" ");
+}
+}
 for (i = 0; i < j; i++)
 {
-																					int c = *(b + o + i);
+int c = *(b + o + i);
 
-																					if (c < 32 || c > 132)
-																					{
-																					c = '.';
+if (c < 32 || c > 132)
+{
+c = '.';
 }
-																					printf("%c", c);
-																					}
+printf("%c", c);
+}
 printf("\n");
 o += 10;
 }
