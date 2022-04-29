73a8ce4f7d317d007b97fd8dfa25d035b1e627ea
Emahniee <101466735+Emahniee@users.noreply.github.com>
2022-04-29 22:10:00 +0100

    Advanced task 1

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,7 +11,7 @@ int long_1(char *s)
 {
 	if (*s != '\0')
 	{
-		return (1 + long_1(s + 1));
+	return (1 + long_1(s + 1));
 	}
 	return (0);
 
@@ -27,15 +27,15 @@ int compare(char *s, int l)
 {
 if (l <= 0)
 	{
-		return (1);
+	return (1);
 	}
 	if (*s == *(s + (l - 1)))
 	{
-		return (compare(s + 1, l - 2));
+	return (compare(s + 1, l - 2));
 	}
 	else
 	{
-		return (0);
+	return (0);
 	}
 }
 
@@ -47,8 +47,8 @@ if (l <= 0)
  */
 int is_palindrome(char *s)
 {
-	int l;
+int l;
 
 l = long_1(s);
-	return (compare(s, l));
+return (compare(s, l));
 }
