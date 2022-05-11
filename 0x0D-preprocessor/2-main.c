fa68400df9f681cead813430488b91b0d7895c48
Nwafor <nwaforemmanuel42@gmail.com>
2022-05-11 19:42:13 +0000

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - write a program that prints the name of the file it was compiled
- * from, followed by a new line
+ * main - Write a program that prints the name of the file it was compiled
+ * from, followed by a new line.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
-
 int main(void)
 {
 printf("%s\n", __FILE__);
-return 0;
+return (0);
 }
