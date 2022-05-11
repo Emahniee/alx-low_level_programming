07b95a665c2293d6ed4443ffdc9bcace0078a6c1
Nwafor <nwaforemmanuel42@gmail.com>
2022-05-11 19:34:20 +0000

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * A program that prints the name of the file it
+ * was compiled from, followed by a new line
+ *
+ * Return: 0
+ */
+
+void main()
+{
+	printf("%s\n", __FILE__)
+		return 0
+}
