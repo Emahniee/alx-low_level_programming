7ba6aa63c19f6ee56fe0c24cb0ee30594a2e5e58
Nwafor <nwaforemmanuel42@gmail.com>
2022-05-13 20:46:33 +0000

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
-#include "function_pointers.h"
 #include <stdio.h>
+#include "function_pointers.h"
 
 /**
  * print_name - prints a name as is
@@ -7,14 +7,14 @@
  * @f: is pointer
  * Return: Nothing.
  */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-	{
-		return;
-	}
-	else
-	{
-		f(name);
-	}
+if (name == NULL || f == NULL)
+{
+return;
+}
+else
+{
+f(name);
+}
 }
