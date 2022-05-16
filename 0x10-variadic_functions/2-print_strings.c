318d3a2185ae3bb34e8160280db0c42427afa51a
Nwafor <nwaforemmanuel42@gmail.com>
2022-05-16 22:38:54 +0000

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,27 +7,28 @@
  *
  * Return: no return.
  */
-void print_strings(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
-	unsigned int i;
-	char *str;
+va_list valist;
+unsigned int i;
+char *str;
 
-	va_start(valist, n);
+va_start(valist, n);
 
-		for (i = 0; i < n; i++)
-	{
-		str = va_arg(valist, char*);
+for (i = 0; i < n; i++)
+{
+str = va_arg(valist, char *);
 
-			if (str)
-				printf("%s", str);
-			else
-				print("(nil)");
-			
-			if (i < n - 1)
-				if (separator)
-					printf("%s", separator);
-	}
-		printf("\n");
-		va_end(valist);
+if (str)
+printf("%s", str);
+else
+printf("(nil)");
+
+if (i < n - 1)
+if (separator)
+printf("%s", separator);
+}
+
+printf("\n");
+va_end(valist);
 }
