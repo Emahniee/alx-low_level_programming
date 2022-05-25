cf1ea78922e75fef4d0c9f7e95a75c42a5e382be
Nwafor <nwaforemmanuel42@gmail.com>
2022-05-25 21:06:28 +0000

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,14 +8,14 @@
  */
 size_t list_len(const list_t *h)
 {
-struct const list_t* current = h;
-int count = 0;
+const list_t *temp;
+unsigned int count = 0;
 
-while (current != NULL)
+temp = h;
+while (temp)
 {
-	count++;
-	current = current -> next;
+count++;
+temp = temp->next;
 }
-
 return (count);
 }
