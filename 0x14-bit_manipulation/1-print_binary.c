d560be7f653a8e1ec571ce654fd9647da239851f
Nwafor <nwaforemmanuel42@gmail.com>
2022-05-30 20:55:03 +0000

    task 1 and 2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,6 @@
+#include "main.h"
 
+/**
  * print_binary - Prints the binary representation of a number.
  * @n: The number to be printed in binary.
  */
@@ -8,3 +10,4 @@ void print_binary(unsigned long int n)
 		print_binary(n >> 1);
 
 	_putchar((n & 1) + '0');
+}
